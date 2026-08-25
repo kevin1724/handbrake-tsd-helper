a6bc735b896d3db2fafee28c93ffb9f33e046ef8
Kevin Ruiz Arzate <104281973+kevin1724@users.noreply.github.com>
2026-08-24 17:22:39 -0700

    Correct iPad workspace assertion

--- mobile/bytesqueeze/test/widget_test.dart
+++ mobile/bytesqueeze/test/widget_test.dart
@@ -45,7 +45,7 @@ void main() {
     await tester.tap(find.text('Library'));
     await tester.pumpAndSettle();
     expect(controller.selectedTab, 1);
-    expect(find.text('YOUR MEDIA'), findsOneWidget);
+    expect(find.text('YOUR MEDIA'), findsWidgets);
   });
 
   testWidgets('V3 command center exposes navigation and safe actions',
